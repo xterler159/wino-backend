1d0713eda268677920ff1d8d6727970e8c0b6ac6
Kevin Fernando <kevin.fernando@supinfo.com>
2019-11-17 12:25:27 +0100

    Can create an excel file

--- dist/controllers/SalesController.js
+++ dist/controllers/SalesController.js
@@ -9,11 +9,28 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const xlsx_1 = require("xlsx");
 const Sales_1 = require("../models/Sales");
+exports.generateExcelFile = () => {
+    const wb = xlsx_1.utils.book_new();
+    const wsData = [["Hello", "World"]];
+    // creating the "workbook"
+    wb.Props = {
+        Title: "SheetJS Tutorial",
+        Subject: "Test",
+        Author: "Red Stapler",
+        CreatedDate: new Date(2017, 12, 19)
+    };
+    wb.SheetNames.push("Test Sheet");
+    const ws = xlsx_1.utils.aoa_to_sheet(wsData);
+    wb.Sheets["Test Sheet"] = ws;
+    xlsx_1.writeFile(wb, "test.xlsx");
+};
 exports.getTotal = () => __awaiter(void 0, void 0, void 0, function* () {
     const sales = yield Sales_1.getSales();
     const count = sales.length; // total of sel
     let total = 0;
+    exports.generateExcelFile();
     sales.map((sale) => {
         total += parseFloat(sale.amount);
     });
--- src/controllers/SalesController.ts
+++ src/controllers/SalesController.ts
@@ -1,9 +1,32 @@
+import fs from "fs";
+import {utils, write, writeFile} from "xlsx";
+
 import {getSales} from "../models/Sales";
+
+export const generateExcelFile = () => {
+    const wb = utils.book_new();
+    const wsData = [["Hello", "World"]];
+
+    // creating the "workbook"
+    wb.Props = {
+        Title: "SheetJS Tutorial",
+        Subject: "Test",
+        Author: "Red Stapler",
+        CreatedDate: new Date(2017, 12, 19)
+    };
+    wb.SheetNames.push("Test Sheet");
+    const ws = utils.aoa_to_sheet(wsData);
+    wb.Sheets["Test Sheet"] = ws;
+
+    writeFile(wb, "test.xlsx");
+};
 
 export const getTotal = async () => {
     const sales = await getSales();
     const count = sales.length; // total of sel
     let total = 0;
+
+    generateExcelFile();
 
     sales.map((sale: any) => {
         total += parseFloat(sale.amount);
